1e70fab90279e380a52637a597990bb1abe46e93
Ravikant99 <mail2ravikant99@gmail.com>
2026-01-08 00:57:44 +0530

    app and main tsx file update to resolve routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Use HashRouter for GitHub Pages compatibility
 import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
