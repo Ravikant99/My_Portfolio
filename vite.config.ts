f4c273de2f493fd27bba45d132106bbf285c58d0
Ravikant99 <mail2ravikant99@gmail.com>
2026-01-08 00:38:55 +0530

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My_Portfolio/',
+  base: "/My_Portfolio/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src") // 👈 this tells Vite that @ = src/
+    }
+  }
 })
